959b2c5694da8ef6c177df3b5500f09321ac9868
Robert Wood Jr <rdrwood@gmail.com>
2020-12-28 20:51:59 -0800

    Add project files.

---
+++ HelloWorldCPPProject.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Starting program...." << flush;
+    cout << "This is some text" << endl;
+    cout << "Banana " << "Apple " << "Orange " << endl;
+
+    cout << "Hello World!\n" << endl;;
+
+    cout << "BYE World!\n" << endl;;
+}
