ab3adf7b968c8d1d355c7623079866975db2fa7a
Robert Wood Jr <rdrwood@gmail.com>
2020-12-28 21:09:38 -0800

    Section 2 Basic Syntax - 7. Variables

--- HelloWorldCPPProject.cpp
+++ HelloWorldCPPProject.cpp
@@ -4,23 +4,30 @@ using namespace std;
 
 int main()
 {
-	int numberCats = 5;
-	int numberDogs = 7;
-	int numberAnimals = numberCats + numberDogs;
-	cout << "I have " << numberCats << " cats and " << flush;
-	cout << "I have " << numberDogs << " dogs." << endl;
-	cout << "Total number of animals: " << numberAnimals << endl;
+    string text1 = "Hello ";
+    string text2 = "Fred";
+    string text3 = text1 + text2;
+    cout << text1 << text2 << endl;
 
-	cout << "New dog acquired!" << endl;
-	numberDogs = numberDogs++;
-	cout << "New number of dogs: " << numberDogs << endl;
+    cout << text3 << endl;
 
-   // cout << "Starting program...." << flush;
-   // cout << "This is some text" << endl;
-   // cout << "Banana " << "Apple " << "Orange " << endl;
+    // int numberCats = 5;
+	// int numberDogs = 7;
+	// int numberAnimals = numberCats + numberDogs;
+	// cout << "I have " << numberCats << " cats and " << flush;
+	// cout << "I have " << numberDogs << " dogs." << endl;
+	// cout << "Total number of animals: " << numberAnimals << endl;
 
-   // cout << "Hello World!\n" << endl;
+    // cout << "New dog acquired!" << endl;
+    // numberDogs = numberDogs++;
+    // cout << "New number of dogs: " << numberDogs << endl;
 
-   // cout << "BYE World!\n" << endl;
+    // cout << "Starting program...." << flush;
+    // cout << "This is some text" << endl;
+    // cout << "Banana " << "Apple " << "Orange " << endl;
+
+    // cout << "Hello World!\n" << endl;
+
+    // cout << "BYE World!\n" << endl;
     return 0;
 }
