78c10e2c1a728427e5e50935d4716423eedfbd3e
Robert Wood Jr <rdrwood@gmail.com>
2020-12-28 21:12:54 -0800

    Section 2 Basic Syntax - 8. User Input

--- HelloWorldCPPProject.cpp
+++ HelloWorldCPPProject.cpp
@@ -4,12 +4,24 @@ using namespace std;
 
 int main()
 {
-    string text1 = "Hello ";
-    string text2 = "Fred";
-    string text3 = text1 + text2;
-    cout << text1 << text2 << endl;
 
-    cout << text3 << endl;
+    cout << "Enter your name: " << flush;
+    string input;
+    cin >> input;
+    cout << "You entered: " << input << endl;
+
+    cout << "Enter a number: " << flush;
+    int value;
+    cin >> value;
+    cout << "You entered: " << value << endl;
+
+
+    // string text1 = "Hello ";
+    // string text2 = "Fred";
+    // string text3 = text1 + text2;
+    // cout << text1 << text2 << endl;
+
+    // cout << text3 << endl;
 
     // int numberCats = 5;
 	// int numberDogs = 7;
