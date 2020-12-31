9916e65ad02aa1e988eea0e7044a0e5293be9daa
Robert Wood Jr <rdrwood@gmail.com>
2020-12-30 21:40:02 -0800

    break continue

--- HelloWorldCPPProject.cpp
+++ HelloWorldCPPProject.cpp
@@ -7,9 +7,26 @@ using namespace std;
 
 int main()
 {
-    for (int i = 0; i < 10; i++) {
+    const string password = "hello";
+    string input;
+    do {
+        cout << "Enter your password > " << flush;
+        cin >> input;
+        if (input == password) {
+            break;
+        }
+        else {
+            cout << "Access denied" << endl;
+        }
+    } while (true);
+    cout << "Password accepted" << endl;
+    cout << "Program quitting" << endl;
+
+
+
+   /* for (int i = 0; i < 10; i++) {
         cout << "Hello " << i << endl;
-    }
+    }*/
 
     /*const string password = "hello";
     string input;
