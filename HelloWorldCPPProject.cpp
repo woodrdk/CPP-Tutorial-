66f7bf4004a72ac6838796b1a0bd62883a91a17b
Robert Wood Jr <rdrwood@gmail.com>
2020-12-30 22:03:50 -0800

    size of multidimensional arrays

--- HelloWorldCPPProject.cpp
+++ HelloWorldCPPProject.cpp
@@ -7,14 +7,25 @@ using namespace std;
 
 int main()
 {
-    int values[] = { 4,7,3,4 };
-    cout << sizeof(values) << endl;
-    cout << sizeof(int) << endl;
-
-    for (unsigned int i = 0; i < sizeof(values) / sizeof(int); i++) {
-        cout << values[i] << " " << flush;
+    string animals[][3] = {
+        {"fox", "dog", "cat"},
+        {"moose", "squirrel", "parrot"}
+    };
+    for (unsigned int i = 0; i < sizeof(animals)/sizeof(animals[0]); i++) {
+        for (unsigned int j = 0; j < sizeof(animals[0])/sizeof(string); j++) {
+            cout << animals[i][j] << " " << flush;
+        }
     }
-    cout << endl;
+
+
+    //int values[] = { 4,7,3,4 };
+    //cout << sizeof(values) << endl;
+    //cout << sizeof(int) << endl;
+
+    //for (unsigned int i = 0; i < sizeof(values) / sizeof(int); i++) {
+    //    cout << values[i] << " " << flush;
+    //}
+    //cout << endl;
 
 
     //string animals[][3] = {
