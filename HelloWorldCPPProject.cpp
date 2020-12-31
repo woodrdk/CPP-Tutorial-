b38b02725be33f87369c7a6122832c009c4038af
Robert Wood Jr <rdrwood@gmail.com>
2020-12-30 21:51:12 -0800

    arrays

--- HelloWorldCPPProject.cpp
+++ HelloWorldCPPProject.cpp
@@ -7,20 +7,48 @@ using namespace std;
 
 int main()
 {
-    const string password = "hello";
-    string input;
-    do {
-        cout << "Enter your password > " << flush;
-        cin >> input;
-        if (input == password) {
-            break;
-        }
-        else {
-            cout << "Access denied" << endl;
-        }
-    } while (true);
-    cout << "Password accepted" << endl;
-    cout << "Program quitting" << endl;
+
+    cout << "Array of integers" << endl;
+    cout << "=================" << endl;
+    int values[3];
+    values[0] = 88;
+    values[1] = 123;
+    values[2] = 7;
+
+    cout << values[0] << endl;
+    cout << values[1] << endl;
+    cout << values[2] << endl;
+
+    cout << endl << "Array of doubles" << endl;
+    cout << "=================" << endl;
+    double numbers[4] = { 4.5, 2.3, 7.2, 8.1 };
+    for (int i = 0; i < 4; i++) {
+        cout << "Elements at index " << i << ": " << numbers[i] << endl;
+    }
+    cout << endl << "Initializing with zero values" << endl;
+    cout << "=================" << endl;
+    int numberArray[8] = {};
+    for (int i = 0; i < 8; i++) {
+        cout << "Element at index " << i << ": " << numberArray[i] << endl;
+    }
+    string texts[] = { "Apple", "Banana", "Orange" };
+    for (int i = 0; i < 3; i++) {
+        cout << "Element at index " << i << ": " << texts[i] << endl;
+    }
+    //const string password = "hello";
+    //string input;
+    //do {
+    //    cout << "Enter your password > " << flush;
+    //    cin >> input;
+    //    if (input == password) {
+    //        break;
+    //    }
+    //    else {
+    //        cout << "Access denied" << endl;
+    //    }
+    //} while (true);
+    //cout << "Password accepted" << endl;
+    //cout << "Program quitting" << endl;
 
 
 
