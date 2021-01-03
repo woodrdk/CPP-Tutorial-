edde3be36be0560996f96a05ae87bb6436c4c2b3
Robert Wood Jr <rdrwood@gmail.com>
2021-01-02 20:48:17 -0800

    pointer string reversal

--- HelloWorldCPPProject.cpp
+++ HelloWorldCPPProject.cpp
@@ -129,32 +129,48 @@ void manipulate(double *pValue) {
 
 int main()
 {
-    int count = 0;
-    for (int i = 0; i <= 10000; i++) {
-        if (i > 0 && i % 100 == 0) {
-            cout << "." << flush;
-            count++;
-        }
+    char text[] = "hello";
+    int nChars = sizeof(text)-1;
+    char* pStart = text;
+    char* pEnd = text + nChars-1;
+    while (pStart < pEnd) {
+        char save = *pStart;
+        *pStart = *pEnd;
+        *pEnd = save;
+        pStart++;
+        pEnd--;
     }
-    cout << endl << count << endl << endl;
-    double value1 = (double)7 / 2;
-    cout << value1 << endl;
-    int value2 = (int)7.3;
-    cout << value2 << endl;
 
-    int value3 = 8;
-    value3 += 1; // equals 8 + 1
-    cout << value3 << endl;
+    cout << text << endl;
 
-    int value4 = 10;
-    value4 /= 5;
-    cout << value4 << endl;
 
-    int value5 = 12 % 5;
-    cout << value5 << endl;
 
-    double equation = 5.3 / 4 + 2 * 6;
-    cout << equation << endl;
+    //int count = 0;
+    //for (int i = 0; i <= 10000; i++) {
+    //    if (i > 0 && i % 100 == 0) {
+    //        cout << "." << flush;
+    //        count++;
+    //    }
+    //}
+    //cout << endl << count << endl << endl;
+    //double value1 = (double)7 / 2;
+    //cout << value1 << endl;
+    //int value2 = (int)7.3;
+    //cout << value2 << endl;
+
+    //int value3 = 8;
+    //value3 += 1; // equals 8 + 1
+    //cout << value3 << endl;
+
+    //int value4 = 10;
+    //value4 /= 5;
+    //cout << value4 << endl;
+
+    //int value5 = 12 % 5;
+    //cout << value5 << endl;
+
+    //double equation = 5.3 / 4 + 2 * 6;
+    //cout << equation << endl;
 
 
     //int nValue = 8;
